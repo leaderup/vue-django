4dc7388d78563a48fa453c4a6e00ce7d1a40a307
wushuang5112 <6307630+wushuang5112@users.noreply.github.com>
2019-11-19 18:13:52 +0800

    处理第三方静态文件

--- template/app/views.py
+++ template/app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def redirect_staticfiles(request):
+    next_url = request.path_info
+    redirect_url = next_url.replace('/static-vuedj/', '/staticfiles/')
+    return HttpResponseRedirect(redirect_url)
